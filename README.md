e37a1ddb1c3a3cf846c1ca10816b036756e7f4dc
Daniel Bluhm <dbluhm@pm.me>
2021-10-15 17:28:32 -0400

    feat: accept connection info or id in client
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- README.md
+++ README.md
@@ -2,3 +2,32 @@
 
 The echo agent is a containerized static agent. This can be useful in a number
 of scenarios, such as full agent testing and development.
+
+## Running the Echo Agent
+
+```sh
+$ docker run --rm -it -p 3000:80 dbluhm/echo-agent:latest
+```
+
+## Interacting with the Echo Agent
+
+After starting up the agent, you can use `http://localhost:3000/docs` to
+interact with the agent through an OpenAPI UI. A client may be generated from
+the `openapi.json` also provided at `http://localhost:3000/docs/openapi.json`.
+
+A native python client is included in this project.
+
+### Python Client
+
+```python
+
+from echo_agent import EchoClient, ConnectionInfo
+
+async def main():
+	async with EchoClient(base_url="http://localhost:3000") as echo:
+		conn: ConnectionInfo = await echo.new_connection(
+			seed="00000000000000000000000000000000",
+			their_vk="<some verkey value>",
+			endpoint="http://example.com"
+		)
+```
--- echo_agent/client.py
+++ echo_agent/client.py
@@ -7,6 +7,7 @@ from pydantic.tools import parse_obj_as
 
 from .models import ConnectionInfo, NewConnection
 
+
 class EchoClientError(Exception):
     """General echo client error."""
 
@@ -17,6 +18,7 @@ class NoOpenClient(EchoClientError):
 
 class EchoClient:
     """Interact with a remote echo agent."""
+
     def __init__(self, base_url: str):
         self.base_url = base_url
         self.client: Optional[AsyncClient] = None
@@ -34,12 +36,13 @@ class EchoClient:
         self, seed: Union[str, bytes], endpoint: str, their_vk: str
     ) -> ConnectionInfo:
         if not self.client:
-            raise NoOpenClient("No client has been opened; use `async with echo_client`")
+            raise NoOpenClient(
+                "No client has been opened; use `async with echo_client`"
+            )
 
         response = await self.client.post(
-            "/connection", json=NewConnection(
-                seed=seed, endpoint=endpoint, their_vk=their_vk
-            ).dict()
+            "/connection",
+            json=NewConnection(seed=seed, endpoint=endpoint, their_vk=their_vk).dict(),
         )
 
         if not response.is_error:
@@ -49,43 +52,67 @@ class EchoClient:
 
     async def receive_message(self, packed_message: bytes):
         if not self.client:
-            raise NoOpenClient("No client has been opened; use `async with echo_client`")
+            raise NoOpenClient(
+                "No client has been opened; use `async with echo_client`"
+            )
 
-        response = await self.client.post(
-            "/receive", content=packed_message
-        )
+        response = await self.client.post("/receive", content=packed_message)
 
         if response.is_error:
             raise EchoClientError("Failed to receive message")
 
-    async def retrieve_messages(self, connection_id: str) -> List[Message]:
+    async def retrieve_messages(
+        self, connection: Union[str, ConnectionInfo]
+    ) -> List[Message]:
         if not self.client:
-            raise NoOpenClient("No client has been opened; use `async with echo_client`")
+            raise NoOpenClient(
+                "No client has been opened; use `async with echo_client`"
+            )
 
+        connection_id = (
+            connection if isinstance(connection, str) else connection.connection_id
+        )
         response = await self.client.get(f"/retrieve/{connection_id}")
 
         if response.is_error:
             raise EchoClientError("Failed to retrieve messages")
-        
+
         return parse_obj_as(List[Message], response.json())
 
     async def wait_for_message(
-        self, connection_id: str, thid: Optional[str] = None, msg_type: Optional[str] = None
+        self,
+        connection: Union[str, ConnectionInfo],
+        thid: Optional[str] = None,
+        msg_type: Optional[str] = None,
     ) -> Message:
         if not self.client:
-            raise NoOpenClient("No client has been opened; use `async with echo_client`")
+            raise NoOpenClient(
+                "No client has been opened; use `async with echo_client`"
+            )
 
-        response = await self.client.get(f"/wait-for/{connection_id}", params={"thid": thid, "msg_type": msg_type})
+        connection_id = (
+            connection if isinstance(connection, str) else connection.connection_id
+        )
+        response = await self.client.get(
+            f"/wait-for/{connection_id}", params={"thid": thid, "msg_type": msg_type}
+        )
 
         if response.is_error:
             raise EchoClientError("Failed to wait for message")
 
         return Message.parse_obj(response.json())
 
-    async def send_message(self, connection_id: str, message: Message):
+    async def send_message(
+        self, connection: Union[str, ConnectionInfo], message: Message
+    ):
         if not self.client:
-            raise NoOpenClient("No client has been opened; use `async with echo_client`")
+            raise NoOpenClient(
+                "No client has been opened; use `async with echo_client`"
+            )
 
+        connection_id = (
+            connection if isinstance(connection, str) else connection.connection_id
+        )
         response = await self.client.post(f"/send/{connection_id}", json=message)
 
         if response.is_error:
