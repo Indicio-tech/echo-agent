9d2e602d57159c1d646951502e747b2f6d9ba005
Daniel Bluhm <dbluhm@pm.me>
2021-10-19 13:25:43 -0400

    feat: make httpx and fastapi extras for client or server
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- echo_agent/__init__.py
+++ echo_agent/__init__.py
@@ -1,15 +1,28 @@
-from .app import app
-from .client import EchoClient, EchoClientError, NoOpenClient
-from .models import NewConnection, ConnectionInfo
+import logging
 from aries_staticagent.message import Message
 
+from .models import ConnectionInfo, NewConnection
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+try:
+    from .app import app
+except ModuleNotFoundError:
+    LOGGER.warning("Server dependencies not found; install extra `server` if needed")
+
+try:
+    from .client import EchoClient, EchoClientError, NoOpenClient
+except ModuleNotFoundError:
+    LOGGER.warning("Client dependencies not found; install extra `client` if needed")
 
 __all__ = [
+    "NewConnection",
     "ConnectionInfo",
+    "Message",
     "EchoClient",
     "EchoClientError",
-    "Message",
-    "NewConnection",
     "NoOpenClient",
     "app",
 ]
